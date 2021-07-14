c5250dbecbd08493ac7b944427a30158a86c650e
Joanantha Matheus <jmvg.comp@gmail.com>
2021-07-14 01:11:37 -0300

    Safe call, elivs operator e teste de null pointer

---
+++ src/main/kotlin/ChamadaSegura.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val a: Int? = null; //safe call operator
+    println(a?.dec())
+
+    //Elvis operator
+    val opcional: String? = null;
+    val obrigatorio: String = opcional ?: "Valor padrão";
+
+    println(obrigatorio)
+
+    //Forçando um null pointer exception - test
+    val testeNullPointer: Int? = null;
+    println(testeNullPointer?.inc())
+    println(testeNullPointer!!.inc()) //forçando chamada
+
+}
